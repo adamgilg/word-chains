360469bae8a53b7a4ba96b51c0cde3fcc94c3c3f
App Academy <adam.gilg@gmail.com>
2013-01-14 17:13:37 -0800

    Valid dictionary method added

--- word_chains.rb
+++ word_chains.rb
@@ -1,3 +1,15 @@
 class WordChains
+  def valid_dictionary(target_word)
+    words = []
+    File.readlines("2of12inf.txt").each do |line|
+      words << line if line.chomp.size == target_word.size
+    end
+    words
+  end
+
+
+  def adjacent_words
+
+  end
 
 end
